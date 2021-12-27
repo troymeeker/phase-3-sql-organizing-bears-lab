4f91a0cf64599ea25b3eaff34e7aa3e5acdc5dbd
troymeeker <troymeeker1@gmail.com>
2021-12-27 12:13:22 -0800

    commit tree 37c198be82b4294c2d374d6f3922c0f02d4a21df

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears VALUES (1, 'Mr.Chocolate', 2, 'M', 'Brown', 'nice', true);
+
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'F', 'Brown', 'intense', 1);
+
+INSERT INTO bears VALUES (3, 'Tabitha', 4, 'F', 'Brown', 'chill', 1);
+
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 4, 'M', 'Brown', 'mellow', 1);
+
+INSERT INTO bears VALUES (5, 'Melissa', 4, 'F', 'Brown', 'rowdy', 1);
+
+INSERT INTO bears VALUES (6, 'Grinch', 4, 'M', 'Brown', 'loud', 1);
+
+INSERT INTO bears VALUES (7, 'Rowdy', 4, 'F', 'Brown', 'rowdy', 1);
+
+INSERT INTO bears VALUES (8, null, 4, 'F', 'Black', 'hungry', 0);
